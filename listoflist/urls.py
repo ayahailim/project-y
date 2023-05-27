2372986db79a44eb96e93f364c146d62e7ce49f8
ayahailim <asdasd2001.am@gmail.com>
2023-05-27 23:01:36 +0300

    change endpoint from doctors to city

--- listoflist/urls.py
+++ listoflist/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
 
-    path("api/doctors/<int:pk>/", views.citydetailView.as_view(), name="city_detail"),
+    path("api/city/<int:pk>/", views.citydetailView.as_view(), name="city_detail"),
 
 ]
