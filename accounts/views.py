673ea901fe0a546800d375506e0acf95b331e2de
ayahailim <asdasd2001.am@gmail.com>
2023-06-02 07:10:20 -0700

    change login to username and password

--- accounts/views.py
+++ accounts/views.py
@@ -46,6 +46,39 @@ from rest_framework import serializers
 from knox.views import LoginView as KnoxLoginView
 
 class CustomAuthTokenSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        if username and password:
+            user = authenticate(request=self.context.get('request'),
+                                username=username, password=password)
+            if not user:
+                raise serializers.ValidationError('Incorrect username or password.')
+        else:
+            raise serializers.ValidationError('Both username and password are required.')
+
+        attrs['user'] = user
+        return attrs
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = CustomAuthTokenSerializer
+
+    def post(self, request, format=None):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
+'''from django.contrib.auth import authenticate
+from rest_framework import serializers
+from knox.views import LoginView as KnoxLoginView
+
+class CustomAuthTokenSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
 
@@ -73,7 +106,7 @@ class LoginAPI(KnoxLoginView):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         login(request, user)
-        return super(LoginAPI, self).post(request, format=None)
+        return super(LoginAPI, self).post(request, format=None)'''
 # login page
 '''class LoginAPI(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
