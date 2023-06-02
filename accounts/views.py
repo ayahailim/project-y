d1268cdca2bdfc0ffb3d8e99a958ecf7ebb7172e
ayahailim <asdasd2001.am@gmail.com>
2023-06-02 07:27:17 -0700

    login with email and pass

--- accounts/views.py
+++ accounts/views.py
@@ -41,43 +41,9 @@ class RegisterAPI(CreateAPIView):
         status_code = status.HTTP_200_OK
         return Response(response, status=status_code)
 #---------------------------------------------------------------------------------------------------
+from rest_framework import serializers
+from knox.views import LoginView as KnoxLoginView
 from django.contrib.auth import authenticate
-from rest_framework import serializers
-from knox.views import LoginView as KnoxLoginView
-
-class CustomAuthTokenSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
-
-    def validate(self, attrs):
-        username = attrs.get('username')
-        password = attrs.get('password')
-
-        if username and password:
-            user = authenticate(request=self.context.get('request'),
-                                username=username, password=password)
-            if not user:
-                raise serializers.ValidationError('Incorrect username or password.')
-        else:
-            raise serializers.ValidationError('Both username and password are required.')
-
-        attrs['user'] = user
-        return attrs
-
-class LoginAPI(KnoxLoginView):
-    permission_classes = (permissions.AllowAny,)
-    serializer_class = CustomAuthTokenSerializer
-
-    def post(self, request, format=None):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        login(request, user)
-        return super(LoginAPI, self).post(request, format=None)
-'''from django.contrib.auth import authenticate
-from rest_framework import serializers
-from knox.views import LoginView as KnoxLoginView
-
 class CustomAuthTokenSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
@@ -103,10 +69,16 @@ class LoginAPI(KnoxLoginView):
 
     def post(self, request, format=None):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except serializers.ValidationError as e:
+            return Response({'error': e.detail}, status=status.HTTP_400_BAD_REQUEST)
+
         user = serializer.validated_data['user']
         login(request, user)
-        return super(LoginAPI, self).post(request, format=None)'''
+        return super(LoginAPI, self).post(request, format=None)
+    
 # login page
 '''class LoginAPI(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
