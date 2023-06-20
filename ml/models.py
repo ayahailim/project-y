c70c50bac7377f9dacd8ad1344613dc6cdc35387
ayahailim <asdasd2001.am@gmail.com>
2023-06-20 09:58:54 -0700

    cairo time zone

--- ml/models.py
+++ ml/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-'''class classifier(models.Model):
-    image= models.ImageField(upload_to="ml/image/",blank=True)
-    date =models.DateTimeField(auto_now_add=True)
-    prediction = models.CharField(max_length=100,null=False)
-    def __str__(self):
-        return f"{self.user.username}'s image"'''
-
 class preuser(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     image= models.ImageField(upload_to="ml/image/",blank=True)
--- ml/serializers.py
+++ ml/serializers.py
@@ -6,10 +6,18 @@ from .models import preuser
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework import serializers
+import pytz
+from rest_framework import serializers
+from django.utils import timezone
 
+class EgyptDateTimeField(serializers.DateTimeField):
+    def to_representation(self, value):
+        timezone_obj = pytz.timezone('Africa/Cairo')
+        date_with_clock = timezone.localtime(value, timezone_obj)
+        return date_with_clock.strftime('%Y-%m-%d %H:%M:%S %Z')
 
 class preuserSerializer(serializers.ModelSerializer):
-    
+    date = EgyptDateTimeField()
     class Meta:
         model = preuser
         fields = ('id','image','date','prediction')
