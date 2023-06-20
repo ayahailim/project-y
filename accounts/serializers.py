35561aaf2dbb5756c90c7e27062253ed1380bf57
ayahailim <asdasd2001.am@gmail.com>
2023-06-20 11:11:01 -0700

    base

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,7 +7,39 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 User = get_user_model()
 
+from django.core.files.base import ContentFile
+import base64
+import six
+import uuid
+
 class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, six.string_types):
+            # Check if the base64 data is empty
+            if data.strip() == '':
+                return None
+
+            try:
+                decoded_file = base64.b64decode(data)
+            except TypeError:
+                self.fail('invalid_image')
+
+            # Generate file name:
+            file_name = str(uuid.uuid4())[:12] # 12 characters are more than enough.
+            # Get the file name extension:
+            file_extension = self.get_file_extension(file_name, decoded_file)
+            complete_file_name = "%s.%s" % (file_name, file_extension, )
+
+            data = ContentFile(decoded_file, name=complete_file_name)
+
+        return super(Base64ImageField, self).to_internal_value(data)
+
+    def get_file_extension(self, file_name, decoded_file):
+        extension = imghdr.what(file_name, decoded_file)
+        extension = "jpg" if extension == "jpeg" else extension
+
+        return extension
+'''class Base64ImageField(serializers.ImageField):
     def to_internal_value(self, data):
         from django.core.files.base import ContentFile
         import base64
@@ -44,7 +76,7 @@ class Base64ImageField(serializers.ImageField):
         extension = imghdr.what(file_name, decoded_file)
         extension = "jpg" if extension == "jpeg" else extension
 
-        return extension
+        return extension'''
 #---------------------------------------------------------------------------------------------------------------
 class profileSerializer(serializers.ModelSerializer):
     profile_pic = Base64ImageField(max_length=None, use_url=True, )
