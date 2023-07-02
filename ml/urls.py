e71af4f5f98bb4a1bdae9244cb390fbd482c39f5
ayahailim <asdasd2001.am@gmail.com>
2023-07-02 09:35:23 -0700

    tf

--- ml/urls.py
+++ ml/urls.py
@@ -1,9 +1,10 @@
 
 from ml import views
-from .views import classAPIView
+from .views import classAPIView,classAPIViewtf
 from django.urls import path
 
 urlpatterns = [
     
     path('predict/', classAPIView.as_view(), name='predict the disease'),
+    path('predicttf/', classAPIViewtf.as_view(), name='predict the disease')
 ]
--- ml/views.py
+++ ml/views.py
@@ -84,4 +84,47 @@ class classAPIView(APIView):
                 return Response({'Disease': prediction_label}, status=200)
         else:
             return Response({'error': 'No image file provided.'}, status=400) 
-        
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+
+class classAPIViewtf(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
+        user = request.user
+        preuser_objects = preuser.objects.filter(user=user)
+        serializer = preuserSerializer(preuser_objects, many=True)
+        return Response(serializer.data)
+    
+    def post(self,request,format=None):
+        classes = ['Basal Cell Carcinoma (BCC)','Melanocytic Nevi (NV)','Melanoma','chicken Pox','Ringworm','Warts Molluscum,Viral Infections','normal']
+        image_file = request.FILES.get('image')
+        user = request.user
+        if image_file:
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(image_file.read())
+                temp_file.flush()
+                # Load the TFLite model
+                interpreter = tf.lite.Interpreter(model_path='./ml/tflite_model.tflite')
+                interpreter.allocate_tensors()
+                # Load the image and preprocess it
+                test_image = load_img(temp_file.name, target_size=(224, 224))
+                test_image = img_to_array(test_image) / 255
+                test_image = np.expand_dims(test_image, axis=0)
+                # Set the input tensor to the interpreter
+                input_details = interpreter.get_input_details()
+                interpreter.set_tensor(input_details[0]['index'], test_image)
+                # Invoke the interpreter to obtain the predictions
+                interpreter.invoke()
+                # Get the output tensor from the interpreter
+                output_details = interpreter.get_output_details()
+                output_data = interpreter.get_tensor(output_details[0]['index'])
+                # Post-process the output data
+                prediction_label = classes[np.argmax(output_data)]
+                preuser.objects.create(user=user,image=image_file, prediction=prediction_label)
+                return Response({'Disease': prediction_label}, status=200)
+        else:
+            return Response({'error': 'No image file provided.'}, status=400)
