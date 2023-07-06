fc8ad3501351e149cdf4b388f46f16aa1a0d3374
ayahailim <asdasd2001.am@gmail.com>
2023-07-06 05:56:27 -0700

    todolist

--- todolist/views.py
+++ todolist/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from rest_framework.permissions import IsAuthenticated
+from knox.auth import TokenAuthentication
 # Create your views here.
 from rest_framework import generics
 from .models import Task
@@ -8,7 +9,11 @@ from .serializers import TaskSerializer
 class TaskList(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    authentication_classes=[TokenAuthentication]
+    permission_classes = (IsAuthenticated,)
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    authentication_classes=[TokenAuthentication]
+    permission_classes = (IsAuthenticated,)
