b2c6bfa2e5fe2878e82e9f928a15b10f5b3a40f7
ayahailim <asdasd2001.am@gmail.com>
2023-07-06 10:11:32 -0700

    search task

--- todolist/views.py
+++ todolist/views.py
@@ -5,8 +5,35 @@ from knox.auth import TokenAuthentication
 from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
+from django.db.models import Q
 
 class TaskList(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = Task.objects.all()
+        search_query = self.request.query_params.get('q', None)
+        if search_query:
+            queryset = queryset.filter(Q(name__icontains=search_query) | Q(description__icontains=search_query))
+        return queryset
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        queryset = Task.objects.all()
+        search_query = self.request.query_params.get('q', None)
+        if search_query:
+            queryset = queryset.filter(Q(name__icontains=search_query) | Q(description__icontains=search_query))
+        obj = generics.get_object_or_404(queryset, pk=self.kwargs["pk"])
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+'''class TaskList(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     authentication_classes=[TokenAuthentication]
@@ -16,4 +43,4 @@ class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     authentication_classes=[TokenAuthentication]
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)'''
