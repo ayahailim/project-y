ec5856d45f9285689837dd031c9f7092956e682a
ayahailim <asdasd2001.am@gmail.com>
2023-07-07 09:20:05 -0700

    url delet

--- ml/urls.py
+++ ml/urls.py
@@ -6,5 +6,6 @@ from django.urls import path
 urlpatterns = [
     
     path('predict/', classAPIView.as_view(), name='predict the disease'),
+    path('predict/<int:pk>/', classAPIView.as_view(), name='delete the disease'),
     path('predicttf/', classAPIViewtf.as_view(), name='predict the disease')
 ]
