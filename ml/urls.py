11fc67e38ad0eb595373e3b32d3753095d19989c
ayahailim <asdasd2001.am@gmail.com>
2023-07-08 16:23:37 -0700

    k

--- ml/urls.py
+++ ml/urls.py
@@ -5,8 +5,8 @@ from django.urls import path
 
 urlpatterns = [
     
-    path('predict/', classAPIView.as_view(), name='predict the disease'),
+    path('predicth/', classAPIView.as_view(), name='predict the disease'),
     path('predict/<int:id>/', classAPIView.as_view(), name='retrieve or delete a specific operation'),
-    path('predicttf/', classAPIViewtf.as_view(), name='predict the disease'),
+    path('predict/', classAPIViewtf.as_view(), name='predict the disease'),
     
 ]
