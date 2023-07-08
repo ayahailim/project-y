83ac9926d8d1b72faa50003416002fc1c237f056
ayahailim <asdasd2001.am@gmail.com>
2023-07-08 09:43:27 -0700

    c

--- todolist/views.py
+++ todolist/views.py
@@ -8,16 +8,24 @@ from .serializers import TaskSerializer
 from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-'''class TaskList(generics.ListCreateAPIView):
+class TaskList(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = (IsAuthenticated,)
 
-    def get_queryset(self):
+    '''def get_queryset(self):
         queryset = Task.objects.all()
         search_query = self.request.query_params.get('q', None)
         if search_query:
             queryset = queryset.filter(Q(name__icontains=search_query) | Q(description__icontains=search_query))
+        return queryset'''
+    def get_queryset(self):
+        queryset = Task.objects.all()
+        search_query = self.request.query_params.get('q', None)
+        if search_query:
+            queryset = queryset.filter(Q(title__icontains=search_query) | Q(description__icontains=search_query))
+        if self.request.query_params.get('title'):
+            queryset = queryset.filter(title__icontains=self.request.query_params.get('title'))
         return queryset
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -32,9 +40,9 @@ class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
             queryset = queryset.filter(Q(name__icontains=search_query) | Q(description__icontains=search_query))
         obj = generics.get_object_or_404(queryset, pk=self.kwargs["pk"])
         self.check_object_permissions(self.request, obj)
-        return obj'''
+        return obj
 
-class TaskList(generics.ListCreateAPIView):
+'''class TaskList(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     authentication_classes=[TokenAuthentication]
@@ -51,4 +59,4 @@ class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
         instance.delete()
         response = Response({'success': 'Object deleted.'}, status=200)
         response['Cache-Control'] = 'no-cache'
-        return response
+        return response'''
