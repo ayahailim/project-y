1c48e2b3ecc7c1915f8880922889e7d9ae36ac3a
ayahailim <asdasd2001.am@gmail.com>
2023-07-08 09:49:59 -0700

    m

--- todolist/views.py
+++ todolist/views.py
@@ -8,17 +8,12 @@ from .serializers import TaskSerializer
 from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
+
 class TaskList(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = (IsAuthenticated,)
 
-    '''def get_queryset(self):
-        queryset = Task.objects.all()
-        search_query = self.request.query_params.get('q', None)
-        if search_query:
-            queryset = queryset.filter(Q(name__icontains=search_query) | Q(description__icontains=search_query))
-        return queryset'''
     def get_queryset(self):
         queryset = Task.objects.all()
         search_query = self.request.query_params.get('q', None)
@@ -29,18 +24,11 @@ class TaskList(generics.ListCreateAPIView):
         return queryset
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
     serializer_class = TaskSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = (IsAuthenticated,)
 
-    def get_object(self):
-        queryset = Task.objects.all()
-        search_query = self.request.query_params.get('q', None)
-        if search_query:
-            queryset = queryset.filter(Q(name__icontains=search_query) | Q(description__icontains=search_query))
-        obj = generics.get_object_or_404(queryset, pk=self.kwargs["pk"])
-        self.check_object_permissions(self.request, obj)
-        return obj
 
 '''class TaskList(generics.ListCreateAPIView):
     queryset = Task.objects.all()
