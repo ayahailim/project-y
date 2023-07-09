ef1220f8f3653fe0b4e6d758b36f7d54a1c03164
ayahailim <asdasd2001.am@gmail.com>
2023-07-09 04:49:06 -0700

    C

--- todolist/models.py
+++ todolist/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    #description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     completed = models.BooleanField(default=False)
