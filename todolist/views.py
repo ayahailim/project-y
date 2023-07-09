887d28f6e8eb284f3696fdd2fe01c314caf4922d
ayahailim <asdasd2001.am@gmail.com>
2023-07-09 04:43:56 -0700

    v

--- todolist/views.py
+++ todolist/views.py
@@ -9,8 +9,8 @@ from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 
-class TaskList(generics.ListCreateAPIView):
-    queryset = Task.objects.all()
+'''class TaskList(generics.ListCreateAPIView):
+    
     serializer_class = TaskSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = (IsAuthenticated,)
@@ -22,8 +22,22 @@ class TaskList(generics.ListCreateAPIView):
             queryset = queryset.filter(Q(title__icontains=search_query) | Q(description__icontains=search_query))
         if self.request.query_params.get('title'):
             queryset = queryset.filter(title__icontains=self.request.query_params.get('title'))
-        return queryset
+        return queryset'''
+class TaskList(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = (IsAuthenticated,)
 
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Task.objects.filter(user=user)
+        search_query = self.request.query_params.get('q', None)
+        if search_query:
+            queryset = queryset.filter(Q(title__icontains=search_query) | Q(description__icontains=search_query))
+        if self.request.query_params.get('title'):
+            queryset = queryset.filter(title__icontains=self.request.query_params.get('title'))
+        return queryset
+    
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
